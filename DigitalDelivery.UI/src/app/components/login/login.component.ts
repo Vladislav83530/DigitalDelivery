40633bf857fd7f16100a49299cb27860bea5f71b
Vladyslav Petrov <vladyslav.petrov@evinent.com>
2025-02-25 01:44:56 +0200

    [feature-auth] - Added validation for login and sigup forms

--- DigitalDelivery.UI/src/app/components/login/login.component.ts
+++ DigitalDelivery.UI/src/app/components/login/login.component.ts
@@ -1,20 +1,45 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
+import ValidateForm from '../helpers/validate-form';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: '../../../assets/css/common-auth.css'
 })
 export class LoginComponent {
-    public passwordInputType: string = 'password';
-    public eyeIcon: string = 'fa-eye-slash';
-    public isText: boolean = false;
+    passwordInputType: string = 'password';
+    eyeIcon: string = 'fa-eye-slash';
+    isText: boolean = false;
+    loginForm!: FormGroup;
 
-    hideShowPass(): void {
+    constructor(private formBuider: FormBuilder) {
+        this.loginForm = this.formBuider.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+
+    hideShowPass() {
         this.isText = !this.isText;
         this.eyeIcon = this.isText ? 'fa-eye' : 'fa-eye-slash';
         this.passwordInputType = this.isText ? 'text' : 'password';
+    }
+
+    onSubmit() {
+        if (this.loginForm.valid) {
+
+        }
+        else {
+            ValidateForm.validateAllFormsField(this.loginForm);
+            alert("Your form is invalid");
+        }
+    }
+
+    showInputValidation(fieldName: string) {
+        return this.loginForm.controls[fieldName].dirty && this.loginForm.hasError('required', fieldName)
     }
 }
--- DigitalDelivery.UI/src/app/components/signup/signup.component.ts
+++ DigitalDelivery.UI/src/app/components/signup/signup.component.ts
@@ -1,20 +1,47 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import ValidateForm from '../helpers/validate-form';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './signup.component.html',
   styleUrl: '../../../assets/css/common-auth.css'
 })
 export class SignupComponent {
-    public passwordInputType: string = 'password';
-    public eyeIcon: string = 'fa-eye-slash';
-    public isText: boolean = false;
+    passwordInputType: string = 'password';
+    eyeIcon: string = 'fa-eye-slash';
+    isText: boolean = false;
+    signupForm!: FormGroup;
+    
+    constructor(private formBuider: FormBuilder) {
+        this.signupForm = this.formBuider.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
 
     hideShowPass(): void {
         this.isText = !this.isText;
         this.eyeIcon = this.isText ? 'fa-eye' : 'fa-eye-slash';
         this.passwordInputType = this.isText ? 'text' : 'password';
+    }
+
+    onSubmit() {
+            if (this.signupForm.valid) {
+    
+            }
+            else {
+                ValidateForm.validateAllFormsField(this.signupForm);
+                alert("Your form is invalid");
+            }
+        }
+    
+    showInputValidation(fieldName: string) {
+        return this.signupForm.controls[fieldName].dirty && this.signupForm.hasError('required', fieldName)
     }
 }
